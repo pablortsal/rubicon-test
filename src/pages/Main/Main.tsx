fe4c7e2e2b245a16c4cf5a2f070f759b3d0d8200
Pablo Ortuño <pablortsal@gmail.com>
2021-07-28 12:29:40 +0200

    Hello World and Basic Structure

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { IonButton, IonDatetime } from "@ionic/react";
+
+export default function Main() {
+    return (
+        <div>
+            <IonButton>DEtial</IonButton>
+        </div>
+    );
+}
